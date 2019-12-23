bbfab6719ac007c9827b8f9b10c2fd4d0a6356ae
Izabela Iskra <ia007@interia.pl>
2019-12-23 18:20:26 +0000

    '

---
+++ kodilla-testing2/src/main/java/com/kodilla/testing/TestingMain.java
@@ -0,0 +1,20 @@
+package com.kodilla.testing;
+
+public class TestingMain {
+    public static void main(String[] args) {
+        System.out.println("kodilla testing");
+        Calculator calculator = new Calculator();
+        double addResult = calculator.add(10, 5);
+        double subResult = calculator.sub(10, 5);
+        if (addResult == 15) {
+            System.out.println("add test ok");
+        } else {
+            System.out.println("add test failed");
+        }
+        if (subResult == 5) {
+            System.out.println("sub test ok");
+        } else {
+            System.out.println("sub test failed");
+        }
+    }
+}
---
+++ kodilla-testing2/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -0,0 +1,13 @@
+package com.kodilla.testing.calculator;
+
+public class Calculator {
+
+    public double add(double a, double b) {
+        return a + b;
+    }
+
+    public double sub(double a, double b) {
+        return a - b;
+    }
+}
+
