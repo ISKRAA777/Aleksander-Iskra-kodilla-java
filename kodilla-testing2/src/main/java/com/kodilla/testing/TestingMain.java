432da2d0b130da736e7002d37f5d05329a40be9a
Izabela Iskra <ia007@interia.pl>
2020-01-06 19:44:09 +0000

    task 06.03

--- kodilla-testing2/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing2/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,5 +1,7 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
     public static void main(String[] args) {
         System.out.println("kodilla testing");
---
+++ kodilla-testing2/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
@@ -0,0 +1,9 @@
+package com.kodilla.testing.collection;
+
+import java.util.ArrayList;
+
+public class OddNumbersExterminator {
+    public ArrayList<Integer> exterminate(ArrayList<Integer> numbers){
+        return null;
+    }
+}
