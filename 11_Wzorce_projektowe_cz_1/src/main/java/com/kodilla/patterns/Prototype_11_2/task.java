b4084004c9b95537d2d1a161b6c525a4863df889
Izabela Iskra <ia007@interia.pl>
2020-01-22 10:42:59 +0000

    moduł 11

---
+++ 11_Wzorce_projektowe_cz_1/src/main/java/com/kodilla/patterns/Prototype_11_2/task.java
@@ -0,0 +1,1 @@
+package com.kodilla.patterns.Prototype_11_2;
---
+++ 11_Wzorce_projektowe_cz_1/src/main/java/com/kodilla/patterns/singleton_11_1/SettingsFileEngine.java
@@ -0,0 +1,45 @@
+package com.kodilla.patterns.singleton_11_1;
+
+public final class SettingsFileEngine {
+    private static SettingsFileEngine settingsFileEngineInstance = null;
+    private String fileName = "";
+
+    private SettingsFileEngine() {
+    }
+
+    public static SettingsFileEngine getInstance() {
+        //Zmodyfikowany kod Singletona, uwzględniający zabezpieczenie przed błędami wynikającymi z wielowątkowości
+        if (settingsFileEngineInstance == null) {
+            synchronized(SettingsFileEngine.class) {
+                if (settingsFileEngineInstance == null) {
+                    settingsFileEngineInstance = new SettingsFileEngine();
+                }
+            }
+        }
+        return settingsFileEngineInstance;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void open(final String fileName) {
+        this.fileName = fileName;
+        System.out.println("Opening the settings file");
+    }
+
+    public void close() {
+        this.fileName = "";
+        System.out.println("Closing the settings file");
+    }
+
+    public boolean loadSettings() {
+        System.out.println("Loading settings from file");
+        return true;
+    }
+
+    public boolean saveSettings() {
+        System.out.println("Saving settings to file");
+        return true;
+    }
+}
